dfb34cd12a3d4e6203a1eedda160fe9e2f1eed57
atekien <tekienandrzej@gmail.com>
2023-07-03 22:31:11 +0200

    Mocked DB setup

--- src/app/portfolio/services/portfolio.service.ts
+++ src/app/portfolio/services/portfolio.service.ts
@@ -9,17 +9,22 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class PortfolioService {
   private readonly url = 'placeholder/portfolio';
   private readonly tiingoApiTestUrl = '/tiingo/daily/AAPL/prices?token=81fbec3ff2bc770c68d6827943c921cae61d2c08' // should be done via backend
+  private readonly mockDBUrl = '/atekien/stocks-tracker-server/portfolios/111111'
   portfolio: BehaviorSubject<Portfolio> = new BehaviorSubject<Portfolio>(undefined as unknown as Portfolio);
-
+  readonly headers = new HttpHeaders({'Content-Type':'application/json'});
+  readonly headers2 = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
   constructor(private http: HttpClient) { }
 
   testApi(): void {
-    const headers = new HttpHeaders({'Content-Type':'application/json'});
-    this.http.get<any>(this.tiingoApiTestUrl, {headers}).subscribe((value) => console.log(value));
+
+    this.http.get<any>(this.tiingoApiTestUrl, {headers: this.headers}).subscribe((value) => console.log(value));
   }
 
   getPortfolio(): Observable<Portfolio> {
     return this.portfolio.asObservable();
+  }
+  loadPortfolioMock(): void {
+    this.http.get<any>(this.mockDBUrl, {headers: this.headers2}).pipe(tap((_portfolio: Portfolio) => this.portfolio.next(_portfolio))).subscribe();
   }
 
   loadPortfolio(id: number): void {
